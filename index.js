fbdc2d71768a3089235fc5c056fb82c065d8f111
katamsharukkumar <sharukkatam@gmail.com>
2025-03-01 11:32:38 +0530

    commit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require("express")
 var app =express();
 
+require("dotenv").config()
 
+console.log(process.env.port);
+
+var port =process.env.port || 3008
 
 app.get('/',(req,res) => {
   res.send("this is root api")
